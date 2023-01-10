dd7a9872d7842a6a384bc6a0ded28bbd858d5dd2
Alfahan <ali.farhan160@gmail.com>
2023-01-10 16:46:17 +0700

    Tipe-Data-Character

---
+++ src/TipeDataChar.java
@@ -0,0 +1,11 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char a = 'A';
+        char l = 'L';
+        char i = 'I';
+
+        System.out.println(a);
+        System.out.println(l);
+        System.out.println(i);
+    }
+}

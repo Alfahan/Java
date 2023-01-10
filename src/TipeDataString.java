4caf5d707436964a239f529514a80faa93677e4f
Alfahan <ali.farhan160@gmail.com>
2023-01-10 16:56:53 +0700

    Tipe-Data-String

---
+++ src/TipeDataString.java
@@ -0,0 +1,13 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Mohamad Ali";
+        String lastName = "Farhan";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        System.out.println(fullName);
+    }
+}

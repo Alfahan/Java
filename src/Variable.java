ecfb2295f81a543f89e4394afa65c20f6629efe1
Alfahan <ali.farhan160@gmail.com>
2023-01-10 17:08:59 +0700

    Variable

---
+++ src/Variable.java
@@ -0,0 +1,26 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "Mohamad Ali Farhan";
+
+        System.out.println(name);
+
+        int age = 25;
+        String adress = "Indonesia";
+
+        System.out.println(age);
+        System.out.println(adress);
+
+        name = "Dadang Konelo";
+        System.out.println(name);
+
+        var firstName = "Mohamad";
+        var middleName = "Ali";
+        var lastName = "Farhan";
+
+//        constan (tidak bisa dirubah)
+        final String application = "Belajar Java";
+
+//        application = "Belajar PHP";
+    }
+}

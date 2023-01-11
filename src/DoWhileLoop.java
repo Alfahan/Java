d892a0285c6ebf7a50acacf86d45cb7d108cdb49
Alfahan <ali.farhan160@gmail.com>
2023-01-11 16:51:06 +0700

    Do-While-Loop

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,10 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        var counter = 10;
+
+        while (counter <= 10){
+            System.out.println("Perulangan " + counter);
+            counter++;
+        } while (counter <= 10);
+    }
+}
